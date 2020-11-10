350c558122e50298eca18d86f45139a759b25505
akim1337 <akim.nech43@gmail.com>
2020-11-10 23:31:42 +0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>Спасибо, бро!)</p>
+      <p>Не важно кто мы такие, важно то, какой у нас план</p>
     </div>
   </footer>
 )
